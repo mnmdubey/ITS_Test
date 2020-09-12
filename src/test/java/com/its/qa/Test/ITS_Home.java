f070f9316808b36521c6b33f1e21eb4d42141c03
Manoranjan Dubey <mnmdubey@gmail.com>
2020-09-12 16:03:19 +0530

    Test

---
+++ src/test/java/com/its/qa/Test/ITS_Home.java
@@ -0,0 +1,10 @@
+package com.its.qa.Test;
+
+public class ITS_Home {
+	
+	public static void main(String[] args, CharSequence input)  {
+
+		System.out.println("ITS_Home - Test2");
+		
+	}
+	}
---
+++ src/test/java/com/its/qa/Test/ITS_Login.java
@@ -0,0 +1,19 @@
+package com.its.qa.Test;
+
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+
+public class ITS_Login {
+
+public static void main(String[] args, CharSequence input)  {
+
+	System.out.println("ITS_Login - Test1");
+	
+}
+}
