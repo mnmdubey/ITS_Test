e2b9ed0e211ade67e3cca8707731df50b6e6bf3e
Manoranjan Dubey <58018383+mnmdubey@users.noreply.github.com>
2020-09-13 16:52:27 +0530

    Update ITS_Home.java

--- src/test/java/com/its/qa/Test/ITS_Home.java
+++ src/test/java/com/its/qa/Test/ITS_Home.java
@@ -6,7 +6,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class ITS_Home {
 	
 	public static void main(String[] args)  {
-
+		
+		//Added by MD from GIT - Start
+		System.out.println("ITS_Home - This is Runninbg from GIT Hub");
+		System.out.println("ITS_Home - This is Runninbg from GIT Hub");
+		System.out.println("ITS_Home - This is Runninbg from GIT Hub");
+		//Added by MD from GIT - End
+		
 		for(int i=1; i<5; i++){
 		System.out.println("ITS_Home - Test2");
 		}
